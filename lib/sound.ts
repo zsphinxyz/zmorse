7424af780f13b4f88f41e6e891a36c80bb546e53
z_sphinx <soe.thiha.lwin.5@gmail.com>
2025-03-04 18:53:52 +0630

    refactor MorseAudio class for improved audio control and functionality

--- lib/sound.ts
+++ lib/sound.ts
@@ -1,81 +1,110 @@
 export class MorseAudio {
   adx: AudioContext;
-  osc: OscillatorNode;
-  gain: GainNode;
+  osc?: OscillatorNode;
+  gain?: GainNode;
 
   freq: number;
   speed: number;
 
-  constructor(freq:number, speed:number) {
+  constructor(freq: number, speed: number) {
     this.freq = freq;
     this.speed = speed;
 
     this.adx = new AudioContext();
-    this.osc = this.adx.createOscillator();
-    this.gain = this.adx.createGain();
-
-    this.osc.type = 'sine';
-    this.osc.frequency.value = this.freq;
-    this.gain.connect(this.adx.destination);
-    this.osc.connect(this.gain);
-    this.gain.gain.value = 0;
-    this.osc.start(); 
   }
   
   initialize() {
-    if(this.adx.state == "suspended"){
-      this.adx.resume();
+    this.osc = this.adx.createOscillator();
+    this.gain = this.adx.createGain();
+    this.osc.type = 'sine';
+    this.osc.frequency.value = this.freq;
+    this.osc.connect(this.gain);
+    this.gain.connect(this.adx.destination);
+    this.gain.gain.value = 0;
+    if (this.adx.state == "suspended") {
+      this.osc.start();
     }
+    // this.gain.gain.value = 0.2;
   }
 
-  setSpeed(speedInput:number) {
+  setSpeed(speedInput: number) {
     this.speed = speedInput;
   }
 
-  setFreq(freqInput:number) {
-    this.osc.frequency.value = freqInput;
+  setFreq(freqInput: number) {
+    if(this.osc) {this.osc.frequency.value = freqInput}
   }
 
   play() {
-    this.gain.gain.setTargetAtTime(0.2, 0, 0.001)
+    this.gain && this.gain.gain.setTargetAtTime(0.2, 0, 0.001)
   }
 
   end() {
-    this.gain.gain.setTargetAtTime(0, 0, 0.001)
+    if(this.gain) {
+      this.gain.gain.setTargetAtTime(0, 0, 0.001);
+      this.gain.gain.value = 0;
+    }
   }
 
-  async pauseTime(time:number) {
-    return new Promise((resolve) => setTimeout(resolve, time))
+  async pauseTime(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  stopAudio() {
+    if(this.osc && this.gain) {
+      this.adx.suspend();
+      this.osc.disconnect();
+      this.osc.stop();
+      this.gain.disconnect();
+      this.gain.gain.value = 0;
+    }
+  }
+
+  restartAudio() {
+    if (!this.adx) {
+      this.adx = new AudioContext();
+    }
+    this.adx.resume();
+    this.initialize();
   }
 
   async playDot() {
-    this.play()
+    this.play();
     await this.pauseTime(this.speed);
-    this.end()
+    this.end();
   }
 
   async playDash() {
-    this.play()
-    await this.pauseTime(this.speed+150)
-    this.end()
+    this.play();
+    await this.pauseTime(this.speed * 3);
+    this.end();
   }
 
-  async playMorse(letter:string) {
-    const li = letter.split('')
-
-    for(let i=0; i < li.length; i++) {
-      if(li[i] == '.') {
+  async playMorse(morse: string) {
+    this.initialize();
+    const morseLi = morse.split('');
+    const morseLength = morseLi.length;
+    for (let i = 0; i < morseLength; i++) {
+      if (morseLi[i] == '.') {
         await this.playDot();
       }
       else {
         await this.playDash();
       }
       await this.pauseTime(this.speed);
-    };
+    }
   }
 
-  async playString(word:string) {
-    const li = word.split('')
+  async playMorseString(morseStr: string) {
+    const morseLi = morseStr.split(' ');
+
+    for (let i = 0; i < morseLi.length; i++) {
+      let morseWord = morseLi[i].split('');
+      for (let j = 0; j < morseWord.length; j++) {
+        await this.playMorse(morseWord[j])
+      }
+      await this.pauseTime(this.speed * 2)
+    }
   }
 
 }
