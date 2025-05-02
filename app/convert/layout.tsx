4283ee6397c19c55bbcbadf58611b56e859c9d08
z_sphinx <soe.thiha.lwin.5@gmail.com>
2025-05-02 15:51:20 +0630

    metadata for convert page

---
+++ app/convert/layout.tsx
@@ -0,0 +1,21 @@
+
+export const metadata = {
+  title: "Morse to Text to Audio",
+  description: "Morse to Text to Audio Converter",
+  applicationName: 'zmorse',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['morse to audio', 'morse code audio', 'morse code converter', 'zmorse', 'morse audio'],
+  creator: 'zsphinx',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
